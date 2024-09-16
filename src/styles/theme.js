5147c75b5723685b107138ed3e3e61c75af46387
Shahzada Muhammad Ahmed <shahzada.ahmed1996@gmail.com>
2024-09-16 11:39:04 +0500

    Redux integrated successfully

---
+++ src/styles/theme.js
@@ -0,0 +1,17 @@
+// Note: Material UI theme.js...!
+
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#1976d2',
+        },
+
+        secondary: {
+            main: '#dc004e',
+        },
+    },
+});
+
+export default theme;
